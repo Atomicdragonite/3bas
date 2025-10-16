1f85153504a24460e8bc47341b4bd3321c87cae2
Your New Global Name <amlmekhamer73@gmail.com>
2025-10-16 22:25:13 +0300

    added new tools

--- test.py
+++ test.py
@@ -4,3 +4,7 @@ addition = num1 + num2
 print ("the addition is:", addition)
 mulitplication = num1 * num2
 print ("the multiplication is:", mulitplication)
+subtraction = num2 - num1
+print ("the subtraction is:", subtraction)
+division = num2 / num1
+print ("the division is:")
